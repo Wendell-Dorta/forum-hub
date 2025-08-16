d44f00353f4d6b2579f380b6159dfdc707dbe402
Wendell Dorta <wendelldorta.comercial@gmail.com>
2025-08-16 18:10:49 -0300

    chore: Remove migration V6 pois a correção foi aplicada na V5

--- src/main/resources/db/migration/V5__create-table-respostas.sql
+++ src/main/resources/db/migration/V5__create-table-respostas.sql
@@ -4,13 +4,12 @@ CREATE TABLE respostas (
     mensagem VARCHAR(255) NOT NULL,
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(100) NOT NULL DEFAULT 'ABERTO',
     solucao BOOL NOT NULL DEFAULT 0,
     autor_id BIGINT NOT NULL,
-    curso_id BIGINT NOT NULL,
+    topico_id BIGINT NOT NULL,
 
     PRIMARY KEY(id),
-    CONSTRAINT fk_topico_autor_id FOREIGN KEY(autor_id) REFERENCES usuarios(id),
-    CONSTRAINT fk_topico_curso_id FOREIGN KEY(curso_id) REFERENCES cursos(id)
+    CONSTRAINT fk_resposta_autor_id FOREIGN KEY(autor_id) REFERENCES usuarios(id),
+    CONSTRAINT fk_resposta_topico_id FOREIGN KEY(topico_id) REFERENCES topicos(id)
 
 );
--- src/main/resources/db/migration/V6__remove-campos-extras-de-respostas.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE respostas
-DROP COLUMN status;
-
-ALTER TABLE respostas
-CHANGE COLUMN curso_id topico_id BIGINT NOT NULL;
-
-ALTER TABLE respostas RENAME INDEX fk_topico_curso_id TO fk_resposta_topico_id;
-ALTER TABLE respostas RENAME INDEX fk_topico_autor_id TO fk_resposta_autor_id;
