61c4958e2a1fe3d32cc0d54495ac1135ff5f2712
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-08-16 00:44:37 -0300

    feat(domain): Implementa a entidade Respostas e seu repositório

---
+++ src/main/java/com/oracle_one/desafio/forum_hub/domain/model/Resposta.java
@@ -0,0 +1,52 @@
+package com.oracle_one.desafio.forum_hub.domain.model;
+
+import com.oracle_one.desafio.forum_hub.domain.enums.StatusTopico;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@EntityListeners(AuditingEntityListener.class)
+public class Resposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private  String mensagem;
+
+    @CreatedDate
+    @Column(name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "solucao")
+    private Boolean solucao = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Usuario autor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topico_id")
+    private Topico topico;
+
+
+}
