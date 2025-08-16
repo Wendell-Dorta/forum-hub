273ee056395ed023e433530d94ce75f989efc108
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2025-08-15 23:45:43 -0300

    feat(domain): Implementa a entidade Usuario e seu repositório

---
+++ src/main/java/com/oracle_one/desafio/forum_hub/domain/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.oracle_one.desafio.forum_hub.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "perfil_id")
+    private Perfil perfil;
+
+    private Boolean ativo;
+
+}
