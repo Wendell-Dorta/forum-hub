375812f831f1ee832d5c8baa770a0c8529ce61c3
Wendell Dorta <wendelldorta.comercial@gmail.com>
2025-08-16 17:31:10 -0300

    Revert "feat(api): Configura o Spring Security e expõe o endpoint de autenticação /login"
    This reverts commit 3a501bfe89e59720dbc49895acb9271651497a44.

---
+++ src/main/java/com/oracle_one/desafio/forum_hub/api/controller/AutenticacaoController.java
@@ -0,0 +1,35 @@
+package com.oracle_one.desafio.forum_hub.api.controller;
+
+import com.oracle_one.desafio.forum_hub.api.dto.autenticacao.DadosAutenticacao;
+import com.oracle_one.desafio.forum_hub.api.dto.autenticacao.DadosTokenJWT;
+import com.oracle_one.desafio.forum_hub.domain.model.Usuario;
+import com.oracle_one.desafio.forum_hub.infra.security.TokenService;
+import jakarta.validation.Valid;
+import jakarta.xml.bind.annotation.XmlAnyElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
--- src/main/java/com/oracle_one/desafio/forum_hub/domain/repository/UsuarioRepository.java
+++ src/main/java/com/oracle_one/desafio/forum_hub/domain/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.oracle_one.desafio.forum_hub.domain.repository;
 
 import com.oracle_one.desafio.forum_hub.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
